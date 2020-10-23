89139b27d7b788844826072f138195fabe4302ab
Ana Caroline Sinigaglia <ana.sinigaglia@iteris.com.br>
2020-10-23 16:49:59 -0300

    Finalizacao dos testes

--- Mock-Fetch/mock-fetch-jest-demo-master/src/utils/currency.test.js
+++ Mock-Fetch/mock-fetch-jest-demo-master/src/utils/currency.test.js
@@ -1,15 +1,28 @@
 import { convert } from './currency';
 
-global.fetch = jest.fn(() => {
-  Promise.resolve({
-    json: () => Promise.resolve( { rates: { CAD: 1.42 } } ); //mock do valor de conversão
-  })
-})
-
 describe('Currency', () => {
+  beforeEach( () => { 
+    fetch.mockClear(); 
+  } );
+  
+  global.fetch = jest.fn(() => 
+    Promise.resolve({
+      json: () => Promise.resolve( { rates: { CAD: 1.42 } } ), //mock do valor de conversão
+    })
+  );
 
   it('should convert USD to CAD', async () => { //USD=dolar americano, CAD=dolar canadense
     const rate = await convert("USD", "CAD");
     expect(rate).toEqual(1.42); //como esse valor de conversão sempre muda, acima faço um valor mockado, só pra testar se a conversão ocorre, independente do valor
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle exception with null', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject("API failure"));
+    const rate = await convert("USD", "CAD");
+    expect(rate).toEqual(null);
+    expect(fetch).toHaveBeenCalledWith(
+      `https://api.exchangeratesapi.io/latest?base=USD`
+    );
   })
 })
